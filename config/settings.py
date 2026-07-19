3dd1f848cb86d7dc97f48ea61e8da39342e2476e
Nikita Armbrister <nikritus@gmail.com>
2026-07-19 16:21:34 +0700

    feat(setting): добавление полей с темами вопросов и грейдами кандидата

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,18 @@
+import pandas as pd
+import sqlite3
+from typing import List
 from pydantic import Field
 from pydantic_settings import BaseSettings
+
+DB_PATH = 'interview_questions_db/questions.db'
+conn = sqlite3.connect(DB_PATH)
+questions_themes = pd.read_sql(sql='SELECT DISTINCT type FROM questions;', con=conn)['type'].to_list()
 
 class Settings(BaseSettings):
     # Telegram
     TELEGRAM_BOT_TOKEN: str = Field(title='Токен бота в Телеграме')
     
-    # LLMs
+    # Agents-LLMs
     INTERVIEWER_MODEL_NAME: str = Field(title='Название модели-интервьюера')
     INTERVIEWER_MODEL_API_BASE: str = Field(title='API источника модели-интервьюера')
     INTERVIEWER_MODEL_API_KEY: str = Field(title='API ключ для обращения к модели-интервьюера')
@@ -14,14 +21,14 @@ class Settings(BaseSettings):
     OBSERVER_MODEL_API_BASE: str = Field(title='API источника модели-наблюдателя')
     OBSERVER_MODEL_API_KEY: str = Field(title='API ключ для обращения к модели-наблюдателя')
     
-    SUMMARIZER_MODEL_NAME: str = Field(title='Название модели-фидбэкера')
-    SUMMARIZER_MODEL_API_BASE: str = Field(title='API источника модели-фидбэкера')
-    SUMMARIZER_MODEL_API_KEY: str = Field(title='API ключ для обращения к модели-фидбэкера')
-    
     TEMPERATURE: float = Field(default=0.3, title='Температура модели')
     
-    # Интервью
+    # Параметры интервью
     QUESTIONS_COUNT: int = Field(default=3, title='Максимальное количество вопросов в интервью')
+    QUESTIONS_THEMES: List[str] = Field(default=questions_themes, title='Доступные темы вопросов')
+    
+    # Параметры кандидата
+    AVAILABLE_GRADES: List[str] = Field(default=['Junior', 'Middle', 'Senior', 'Team lead'], title='Доступные грейды кандидата')
     
     class Config:
         env_file = '.env'
